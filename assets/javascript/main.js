f7e4549a99616daffd9c7d96dd4645918667a81e
katie stout <katiestout@katies-MBP.home>
2018-06-02 11:37:54 -0400

    trying to fix functions

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,49 +1,16 @@
-// $( document ).ready(function() {
-
-  //  $("#allthings").hide();});
-
-//$("#enter").click(function(){
-
-  //$("#secondpageclick").show();
-  //$("#firstpageclick").hide();
-  //$("#blendspage").hide();
-//});
-
-
-//$("#blendsbtn").click(function(){
-
-  //$("#secondpageclick").hide();
-  //$("#firstpageclick").hide();
-  //$("#blendspage").show();
-
-
-//});
-
 $( document ).ready(function() {
 
-    $("#secondpageclick").toggle();
-   
-   
-    $("#enter").click(function() {
-  
-      $("#firstpageclick").hide();
-      $("#blendspage").hide();
-      $("#secondpageclick").show();
-  
-      //need to hide secondpageclick untill firstpageclick
-    });
-  
-      $("#blendsbtn").click(function(){
-  
-        $("#firstpageclick").hide();
-        $("#secondpageclick").hide();
-        $("#blendspage").show();
-  
-  
-      });
-  
-  
-    //if you click enter, show secondpage hide third, if you click blends show blendspage hide first and third
-  
-      }); 
-  
+
+  $("#secondpageclick").toggle();
+  $("#blendspage").toggle();
+  $("#firstpageclick").show();
+     
+     
+  $("#enter").on('click', (function() {
+
+    $("#firstpageclick").hide();
+    $("#blendspage").hide();
+    $("#secondpageclick").show();
+}));
+
+});
